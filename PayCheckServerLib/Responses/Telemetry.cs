4b830da54a2dd29fca8f7b56716ad06305f995ed
HW12Dev <26612968+HW12Dev@users.noreply.github.com>
2023-08-13 19:43:10 +1000

    Fix possible unhandled IOException

--- PayCheckServerLib/Responses/Telemetry.cs
+++ PayCheckServerLib/Responses/Telemetry.cs
@@ -1,4 +1,5 @@
 ﻿using NetCoreServer;
+using System.Linq.Expressions;
 
 namespace PayCheckServerLib.Responses
 {
@@ -8,7 +9,11 @@ namespace PayCheckServerLib.Responses
         public static bool ProtectedEvents(HttpRequest request, PC3Server.PC3Session session)
         {
             if (!Directory.Exists("Telemetry")) { Directory.CreateDirectory("Telemetry"); }
-            File.WriteAllText("Telemetry/" + DateTime.Now.ToString("s").Replace(":", "-") + ".json", request.Body);
+            try {
+                File.WriteAllText("Telemetry/" + DateTime.Now.ToString("s").Replace(":", "-") + ".json", request.Body);
+            } catch(IOException e) {
+                Debugger.PrintError(String.Format("Exception occurred while writing telemetry: {0}", e.ToString()));
+            }
             session.SendResponse(session.Response.MakeOkResponse());
             return true;
         }
