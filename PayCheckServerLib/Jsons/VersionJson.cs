3daf8cf09f0d2516fc61272befaad64ba4861be1
HW12Dev <26612968+HW12Dev@users.noreply.github.com>
2023-08-24 21:41:53 +1000

    Add version endpoints

---
+++ PayCheckServerLib/Jsons/VersionJson.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+namespace PayCheckServerLib.Jsons {
+	public class VersionJson {
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("realm")]
+		public string Realm { get; set; }
+
+		[JsonProperty("version")]
+		public string Version { get; set; }
+
+		[JsonProperty("gitTag")]
+		public string GitTag { get; set; }
+
+		[JsonProperty("gitHash")]
+		public string GitHash { get; set; }
+
+		[JsonProperty("gitBranchName")]
+		public string GitBranchName { get; set; }
+
+		[JsonProperty("buildDate")]
+		public string BuildDate { get; set; }
+
+		[JsonProperty("buildID")]
+		public string BuildID { get; set; }
+
+		[JsonProperty("buildBy")]
+		public string BuildBy { get; set; }
+
+		[JsonProperty("buildOS")]
+		public string BuildOS { get; set; }
+
+		[JsonProperty("buildJDK")]
+		public string BuildJDK { get; set; }
+
+		[JsonProperty("version-roles-seeding")]
+		public string VersionRolesSeeding { get; set; }
+	}
+}
---
+++ PayCheckServerLib/Responses/VersionResponse.cs
@@ -0,0 +1,72 @@
+﻿using NetCoreServer;
+using Newtonsoft.Json;
+using PayCheckServerLib.Jsons;
+using System.Text.RegularExpressions;
+
+namespace PayCheckServerLib.Responses {
+	public class VersionResponse {
+		static bool GenericVersionResponse(HttpRequest request, PC3Server.PC3Session session) {
+			ResponseCreator response = new ResponseCreator();
+
+			var regex = new Regex(@"\/(.*)\/version");
+			Debugger.PrintDebug(request.Url);
+			MatchCollection matches = regex.Matches(request.Url);
+			var endpoint = "";
+			foreach (Match match in matches) {
+				foreach (Group group in match.Groups) {
+					if(!group.Value.Contains("/")) {
+						endpoint = group.Value;
+					}
+				}
+			}
+
+			// data taken from https://nebula.starbreeze.com/agreement/version and https://nebula.starbreeze.com/platform/version
+			VersionJson version = new() {
+				Name = endpoint == "agreement" ? "justice-legal-service" : String.Format("justice-{0}-service", endpoint),
+				Realm = "prod",
+				Version = "1.28.0",
+				GitTag = "1.28.0",
+				GitHash = "b55c3fcbc9",
+				GitBranchName = "release-candidate",
+				BuildDate = "2023-03-13T01:07:41+00:00",
+				BuildID = "1.28.0",
+				BuildBy = "Gradle 6.9.1",
+				BuildOS = "Linux amd64 5.13.0-1021-aws",
+				BuildJDK = "1.8.0_232 (Eclipse OpenJ9 openj9-0.17.0)",
+				VersionRolesSeeding = "0.0.3"
+			};
+
+			response.SetBody(JsonConvert.SerializeObject(version));
+			session.SendResponse(response.GetResponse());
+			return true;
+		}
+		[HTTP("GET", "/iam/version")]
+		public static bool IamVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/agreement/version")]
+		public static bool AgreementVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/basic/version")]
+		public static bool BasicVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/platform/version")]
+		public static bool PlatformVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/social/version")]
+		public static bool SocialVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/leaderboard/version")]
+		public static bool LeaderboardVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/achievement/version")]
+		public static bool AchievementVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/cloudsave/version")]
+		public static bool CloudSaveVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/ugc/version")]
+		public static bool UGCVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/lobby/version")]
+		public static bool LobbyVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/group/version")]
+		public static bool GroupVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/qosm/version")]
+		public static bool QOSMVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/dsmcontroller/version")]
+		public static bool DSMControllerVersion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+		[HTTP("GET", "/game-telemetry/version")]
+		public static bool GameTelemetryVerion(HttpRequest request, PC3Server.PC3Session session) { return GenericVersionResponse(request, session); }
+	}
+}
