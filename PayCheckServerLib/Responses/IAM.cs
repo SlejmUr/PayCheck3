df6d586eb02ebad2a9e9c7cca1b77ef9ff6dcdb3
HW12Dev <26612968+HW12Dev@users.noreply.github.com>
2023-09-08 23:41:09 +1000

    Add basic support for the game's login form

--- PayCheckServerLib/Responses/IAM.cs
+++ PayCheckServerLib/Responses/IAM.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using PayCheckServerLib.Helpers;
 using PayCheckServerLib.Jsons;
+using System.Web;
 
 namespace PayCheckServerLib.Responses
 {
@@ -61,9 +62,20 @@ namespace PayCheckServerLib.Responses
             session.SendResponse(response.GetResponse());
             Debugger.PrintDebug("Sent Response!");
             return true;
-        }
 
-        [HTTP("POST", "/iam/v3/oauth/platforms/device/token")]
+			/***
+             * return status code 401 with
+             * {
+                    "clientId": "uuidv4",
+                    "error": "platform_not_linked",
+                    "linkingToken": "uuidv4",
+                    "platformId": "steam"
+                }
+             * to get game to allow email + password auth
+             */
+		}
+
+		[HTTP("POST", "/iam/v3/oauth/platforms/device/token")]
         public static bool DeviceToken(HttpRequest request, PC3Server.PC3Session session)
         {
             var deviceid = request.Body.Split('=')[1];
@@ -105,9 +117,63 @@ namespace PayCheckServerLib.Responses
             response.SetBody(JsonConvert.SerializeObject(token));
             session.SendResponse(response.GetResponse());
             return true;
+		}
+
+		// Logging in with email + password, also links steam to nebula account on official servers
+		[HTTP("POST", "/iam/v3/authenticateWithLink")]
+		public static bool AuthenticateWithLink(HttpRequest request, PC3Server.PC3Session session)
+        {
+            var param = HttpUtility.ParseQueryString(request.Body);
+            // either username or email entered
+            var username = param["username"];
+            // plain text
+            var password = param["password"];
+            var linking_token = param["linkingToken"];
+            var client_id = param["client_id"];
+
+            // request does not have a device id, client_id will do for now
+			var (access_token, refresh_token) = UserController.LoginUser(client_id, TokenHelper.TokenPlatform.Device);
+
+			ResponseCreator response = new();
+			response.SetHeader("Content-Type", "application/json");
+			response.SetHeader("Connection", "keep-alive");
+			response.SetCookie("refresh_token", refresh_token.ToBase64());
+			response.SetCookie("access_token", access_token.ToBase64());
+			LoginToken token = new()
+			{
+				AccessToken = access_token.ToBase64(),
+				Scope = "account commerce social publishing analytics",
+				Bans = new() { },
+				DisplayName = access_token.Name,
+				ExpiresIn = 360000,
+				IsComply = true,
+                // Jflgs is 1 for this request
+				Jflgs = 1,
+				Namespace = "pd3beta",
+				NamespaceRoles = new()
+				{
+					new NamespaceRole()
+					{
+						Namespace = "*",
+						RoleId = "2251438839e948d783ec0e5281daf05"
+					}
+				},
+				Permissions = new() { },
+				PlatformId = "",
+				PlatformUserId = "",
+				RefreshExpiresIn = 86400,
+				RefreshToken = refresh_token.ToBase64(),
+				Roles = new() { "2251438839e948d783ec0e5281daf05" },
+				TokenType = "Bearer",
+				UserId = access_token.UserId
+			};
+			response.SetBody(JsonConvert.SerializeObject(token));
+			session.SendResponse(response.GetResponse());
+			return true;
         }
 
-        [HTTP("GET", "/iam/v3/public/users/me")]
+
+		[HTTP("GET", "/iam/v3/public/users/me")]
         public static bool UsersMe(HttpRequest _, PC3Server.PC3Session session)
         {
             var auth = session.Headers["authorization"].Replace("Bearer ", "");
